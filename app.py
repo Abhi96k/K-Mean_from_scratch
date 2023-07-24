f3ef2b63055dfb9dbde563eea3ea119ba45fae85
ABHISHEK NANGARE <abhisheknangare96k@gmail.com>
2023-07-24 12:18:34 +0530

    changed sample size to 200

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 centroids = [(-5, -5), (5, 5), (-2.5, 2.5), (2.5, -2.5)]
 cluster_std = [1, 1, 1, 1]
 
-X, y = make_blobs(n_samples=100, cluster_std=cluster_std,
+X, y = make_blobs(n_samples=200, cluster_std=cluster_std,
                   centers=centroids, n_features=2, random_state=2)
 
 # plt.scatter(X[:, 0], X[:, 1])
